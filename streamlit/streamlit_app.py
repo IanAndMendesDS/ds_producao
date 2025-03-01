85777e5be6da662c93877249161cb8b7736eb19e
IanAndMendesDS <ian48_44@hotmail.com.br>
2025-03-01 17:15:13 -0300

    frontend changes

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -26,7 +26,64 @@ pg = st.navigation(
     }
 )
 
+# ------ Side Bar ----------
 
-# ------ Run Navigation ------
+# URLs dos seus perfis
+github_url = "https://github.com/IanAndMendesDS/ds_producao"
+linkedin_url = "https://www.linkedin.com/in/ian-andrade/"
+portfolio_url = "https://ianandmendesds.github.io/portifolio_projetos/"
+
+# Adicionando os botões estilizados com FontAwesome na sidebar
+# Adicionando os botões estilizados com FontAwesome na sidebar
+with st.sidebar:
+    st.markdown(f"""
+        <style>
+            .btn {{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                padding: 10px;
+                margin-bottom: 10px;
+                border-radius: 5px;
+                text-decoration: none;
+                font-size: 18px;
+                font-weight: bold;
+                width: 100%;
+                color: white;
+                transition: background 0.3s;
+            }}
+            .github {{ background: #333; }}
+            .linkedin {{ background: #333; }}
+            .portfolio {{ background: #333; }}
+            .btn:hover {{ opacity: 0.8; }}
+            .btn i {{
+                margin-right: 8px;
+                font-size: 20px;
+            }}
+        </style>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+        <a class="btn github" href="{github_url}" target="_blank">
+            <i class="fa-brands fa-github"></i> GitHub
+        </a>
+
+        <a class="btn linkedin" href="{linkedin_url}" target="_blank">
+            <i class="fa-brands fa-linkedin"></i> LinkedIn
+        </a>
+
+        <a class="btn portfolio" href="{portfolio_url}" target="_blank">
+            <i class="fa-solid fa-folder-open"></i> Portfólio
+        </a>
+    """, unsafe_allow_html=True)
+
+# with st.sidebar:
+#    st.subheader('Saiba mais em:')
+#    st.write('\n')
+#    st.link_button('Github', github_url)
+#    st.link_button('LinkedIn', linkedin_url)
+#    st.link_button('Meu Portfolio', portfolio_url)
+
+#------ Run Navigation ------
 
 pg.run()
--- streamlit/views/forecast.py
+++ streamlit/views/forecast.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 from functions import load_dataset, convert_df, negocio, predict
+import time
 
 
 # ----- Dataset Imports ------
-test = pd.read_csv('datasets/test.csv', low_memory=False)
+test = pd.read_csv('datases/test.csv', low_memory=False)
 store = pd.read_csv('datasets/store.csv', low_memory=False)
 train = pd.read_csv('datasets/train.csv', low_memory=False)
 
@@ -21,49 +22,61 @@ if st.button('Previsão'):
     if data != 'error':
         predictions = predict(data)
 
-        d2 = round(predictions[['store', 'prediction']].groupby('store').agg(['mean', 'sum']).reset_index(),2)
-        d2.columns = ['store', 'prediction_mean', 'prediction_sum']
+        if predictions is None:
+            warning_placeholder = st.empty()  
+            timer_placeholder = st.empty()  
+            warning_placeholder.warning("⏳ Estava dormindo, espere um momento até eu acordar...")
 
-        df_treino = train.loc[train['Store'].isin(d2['store'])]
-        df_treino = round(df_treino[['Store', 'Sales']].groupby('Store').mean().reset_index(),2)
-        df_treino.columns = ['store', 'sales_mean']
+            for i in range(40, 0, -1):
+                 timer_placeholder.text(f"Acordando em {i} segundos... 💤")
+                 time.sleep(1)  
 
-        df_final = df_treino.merge(d2, how='left', on='store')
-        df_final['reforma'] = df_final.apply(lambda x: 1 if x['prediction_mean'] > x['sales_mean'] else 0, axis=1)
-
-        df_final = df_final.loc[df_final['reforma'] == 1]
-
-        if df_final.empty:
-            st.write("Infelizmente as lojas selecionadas não atendem aos critérios de realização da reforma")
-
+            warning_placeholder.empty()
+            timer_placeholder.success("🚀 Estou pronto!")
         else:
-            df_final['porcentagem'] = ((df_final['prediction_mean'] / df_final['sales_mean']) - 1)
-        
-            df_final['porcetagem_orcamento'] = df_final['porcentagem'].apply(lambda x : 0.075 if x < 0.025
-                                                                                else 0.1 if x < 0.05
-                                                                                else 0.125)
+            d2 = round(predictions[['store', 'prediction']].groupby('store').agg(['mean', 'sum']).reset_index(),2)
+            d2.columns = ['store', 'prediction_mean', 'prediction_sum']
+
+            df_treino = train.loc[train['Store'].isin(d2['store'])]
+            df_treino = round(df_treino[['Store', 'Sales']].groupby('Store').mean().reset_index(),2)
+            df_treino.columns = ['store', 'sales_mean']
+
+            df_final = df_treino.merge(d2, how='left', on='store')
+            df_final['reforma'] = df_final.apply(lambda x: 1 if x['prediction_mean'] > x['sales_mean'] else 0, axis=1)
+
+            df_final = df_final.loc[df_final['reforma'] == 1]
+
+            if df_final.empty:
+                st.write("Infelizmente as lojas selecionadas não atendem aos critérios de realização da reforma")
+
+            else:
+                df_final['porcentagem'] = ((df_final['prediction_mean'] / df_final['sales_mean']) - 1)
             
-            df_final['orcamento'] = round(df_final['prediction_sum']*df_final['porcetagem_orcamento'],2)
-            
-            df_final = df_final.drop(columns=['reforma','porcentagem'], axis=1)
-            csv = convert_df(df_final)
+                df_final['porcetagem_orcamento'] = df_final['porcentagem'].apply(lambda x : 0.075 if x < 0.025
+                                                                                    else 0.1 if x < 0.05
+                                                                                    else 0.125)
+                
+                df_final['orcamento'] = round(df_final['prediction_sum']*df_final['porcetagem_orcamento'],2)
+                
+                df_final = df_final.drop(columns=['reforma','porcentagem'], axis=1)
+                csv = convert_df(df_final)
 
 
-            st.write('''
-                 Premissas de Negócio para a seleção das lojas:
-                 1. Se a média do faturamento predict_mean for menor do que a média das sales_mean, então não podemos fazer a reforma; caso contrário podemos fazer a reforma.
-                 2. Se a diferença do faturamento previsto for menor do que 2,5%, pode-se utilizar 7,5% do faturamento total para a reforma. Se estiver entre 2,5% e 5%, utiliza-se 10%; se for superior a 5%, utiliza-se 12,5% do faturamento.
-                 
-                 As lojas selecionadas são:
-                 ''')
-            st.write('\n')
-            st.write(df_final)
-            # ------ download -----
-            st.download_button(
-            label="Download CSV",
-            data= csv,
-            file_name='orçamento.csv',
-            mime='text/csv')
+                st.write('''
+                    Premissas de Negócio para a seleção das lojas:
+                    1. Se a média do faturamento predict_mean for menor do que a média das sales_mean, então não podemos fazer a reforma; caso contrário podemos fazer a reforma.
+                    2. Se a diferença do faturamento previsto for menor do que 2,5%, pode-se utilizar 7,5% do faturamento total para a reforma. Se estiver entre 2,5% e 5%, utiliza-se 10%; se for superior a 5%, utiliza-se 12,5% do faturamento.
+                    
+                    As lojas selecionadas são:
+                    ''')
+                st.write('\n')
+                st.write(df_final)
+                # ------ download -----
+                st.download_button(
+                label="Download CSV",
+                data= csv,
+                file_name='orçamento.csv',
+                mime='text/csv')
         
     else:
         st.write('Store not available')
